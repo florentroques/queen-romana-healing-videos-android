35e3870fdb668bc88a4236b5a13044d520311a2d
Florent Roques <florent.roques@mention.com>
2021-11-05 16:59:17 +0100

    Initial commit

---
+++ app/src/main/java/com/queenromana/healingvideos/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.queenromana.healingvideos
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val videoView = findViewById<VideoView>(R.id.videoView)
+
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+
+        val uri: Uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.become_a_healer)
+
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.setOnPreparedListener { mp -> mp.isLooping = true }
+    }
+}
